62c551562d46eef6a4433a85a518455293a45d97
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-11 17:42:14 -0500

    Update backend.tf

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,1 +1,10 @@
+# S3 remote state 
+terraform {
+ backend "s3" {
+    bucket         = "lightfeather-project"  
+    key            = "project/eks"
+    region         = "us-east-2"
+    dynamodb_table = "lightfeather_dynamodb"
 
+ }
+} 
