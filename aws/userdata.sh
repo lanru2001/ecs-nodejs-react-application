6ff4749b0ab99657aeef8b1d3b5b71a2c25c46e5
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-02 18:41:56 -0500

    Update userdata.sh

--- aws/userdata.sh
+++ aws/userdata.sh
@@ -57,6 +57,6 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade -y
-sudo yum install jenkins -y
+sudo yum install jenkins --nobest -y 
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
