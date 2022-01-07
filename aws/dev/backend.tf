c0bd4b69dbbc8221a9188b86c9d92be692b58be9
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-01-07 16:48:52 -0600

    Update backend.tf

--- aws/dev/backend.tf
+++ aws/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
  backend "s3" {
-    bucket         = "tf-remote-bkt"  
+    bucket         = "tf-state-file01"  
     key            = "project/ecs"
     region         = "us-east-2"
     dynamodb_table = "eks_ecommerce_dynamodb"
