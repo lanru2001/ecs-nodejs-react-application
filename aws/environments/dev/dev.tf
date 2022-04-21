f9ddd26d779a60bc42f0b0c99151b5ffea682157
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-04-21 08:54:51 -0700

    Update dev.tf

--- aws/environments/dev/dev.tf
+++ aws/environments/dev/dev.tf
@@ -1,5 +1,13 @@
+# S3 remote state 
+terraform {
+ backend "s3" {
+    bucket         = "tf-state-file01"  
+    key            = "project/ecs"
+    region         = "us-east-2"
+    dynamodb_table = "eks_ecommerce_dynamodb"
 
-
+ }
+}
 
 module "node_ecs". {
 
