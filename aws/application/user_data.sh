47a438e01b07b16c33b4ab7cbf1edd6d9c7d41f0
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2024-03-25 17:25:07 -0500

    Update user_data.sh

--- aws/application/user_data.sh
+++ aws/application/user_data.sh
@@ -56,6 +56,6 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 # Add required dependencies for the jenkins package
 sudo yum install fontconfig java-17-openjdk -y
-sudo yum install jenkins
+sudo yum install jenkins -y 
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
